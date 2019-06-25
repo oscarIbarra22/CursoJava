e00dbdc3c7d017cc619c0f93232cd0935b7a0930
Oscar Ibarra <oibarra@anzen.com.mx>
2019-06-25 00:05:28 -0500

    add package

---
+++ src/holaMundo.java
@@ -0,0 +1,17 @@
+public class holaMundo {
+
+    public static void main(String[] args) {
+
+        //var des = "bye";
+
+        String adios = "adios";
+        String hola = "Hola Mundo";
+        int number = 35;
+
+        System.out.println(adios);
+        System.out.println(hola);
+    }
+}
+
+
+
